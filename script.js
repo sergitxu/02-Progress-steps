7fbd988b821361c45a00255d7bfb7aab8b5fa9be
Sergio Ortiz <sergio.ortiz@visma.com>
2022-03-08 17:55:15 +0100

    Cleaning code

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 const progressSteps = document.querySelectorAll('.progress__step');
 
-
 let currentActive = 1;
 
 console.log(progressSteps);
@@ -24,12 +23,10 @@ prev.addEventListener('click', () => {
 });
 
 function updateProgress() {
-    // progressSteps[currentActive].classList.add('active');
 
     progressSteps.forEach((step, index) => {
         if (index < currentActive) {
             step.classList.add('active');
-            // progress__bar.width = 20%;
         } else {
             step.classList.remove('active');
         }
@@ -44,9 +41,6 @@ function updateProgress() {
     }
 
     const activeSteps = document.querySelectorAll('.active');
-    console.log(activeSteps.length);
     let percentage = (activeSteps.length - 1) / (progressSteps.length - 1) * 100 + '%';
     progress.style.width = percentage;
-
 }
-
